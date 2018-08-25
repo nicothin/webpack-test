3f7eb55dcf88b389bdd8e89f9ef0ac4f95699450
Nikolay Gromov <nicothin@gmail.com>
2018-08-25 18:17:28 +0300

    Start

---
+++ postcss.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: {
+    'autoprefixer': {
+      grid: true,
+    },
+    'css-mqpacker': {
+      sort: true,
+    },
+    'cssnano': {
+      preset: 'default',
+    },
+  },
+}
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: { main: './src/index.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[chunkhash].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'postcss-loader', 'sass-loader']
+        })
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin('dist', {} ),
+    new ExtractTextPlugin(
+      {filename: 'style.[chunkhash].css', disable: false, allChunks: true}
+    ),
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: true,
+      template: './src/index.html',
+      filename: 'index.html'
+    }),
+  ]
+};
